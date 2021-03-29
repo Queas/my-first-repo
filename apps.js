c012ededfdb0bd7b22ca7363a4614792ef1c83e5
Queas <jeremiahmagdael@gmail.com>
2021-03-29 23:22:43 +0800

    my last bran cell in action

---
+++ apps.js
@@ -0,0 +1,6 @@
+const text = document.querySelector(".title");
+const changeColor = document.querySelector(".changeColor");
+
+changeColor.addEventListener("click", function() {
+    alert("Pogi Si Les");
+});
